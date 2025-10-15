83e4563205f2d327c95ba19a033be58baf1b92bb
Aman Anand <aman1001anand@gmail.com>
2025-10-15 20:49:43 +0530

    backend addition

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const userSchema = new Schema({
     firstName:{
         type: String,
-        require: true,
+        required: true,
         minLength:3,
         maxLength:20
     },
@@ -15,7 +15,7 @@ const userSchema = new Schema({
     },
     emailId:{
         type:String,
-        require:true,
+        required:true,
         unique:true,
         trim: true,
         lowercase:true,
@@ -32,7 +32,11 @@ const userSchema = new Schema({
         default: 'user'
     },
     problemSolved:{
-        type:[string]
+        type:[String]
+    },
+    password:{
+        type:String,
+        required: true
     }
 },{
     timestamps:true
